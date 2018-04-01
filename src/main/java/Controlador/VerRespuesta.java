c034962472c4600156383fd3443d8d07e154fa44
daniel <daniel@francisco>
2018-04-01 01:28:22 -0600

    primera iteracion caso de uso ver vistas

--- src/main/java/Controlador/VerRespuesta.java
+++ src/main/java/Controlador/VerRespuesta.java
@@ -5,10 +5,36 @@
  */
 package Controlador;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import modelo.Pregunta;
+import modelo.PreguntaDAO;
+import modelo.Respuesta;
+import modelo.RespuestaDAO;
 /**
  *
  * @author Admin
  */
+
+@ManagedBean
+@RequestScoped
 public class VerRespuesta {
+    private List<Respuesta> respuestas;
+    private Pregunta pregunta; 
     
+
+    public List<Respuesta> getRespuestas() {
+        return respuestas;
+    }
+    
+    public String listarRespuestas(Pregunta p){
+    this.pregunta=p;
+    RespuestaDAO lib = new RespuestaDAO();
+    respuestas= lib.respuestas(pregunta);
+    return "verrespuestas";
+    }
+
 }
+
---
+++ src/main/java/modelo/PreguntaDAO.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+/**
+ *
+ * @author daniel
+ */
+public class PreguntaDAO {
+    private SessionFactory sessionFactory;
+    
+    
+public PreguntaDAO(){
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+public List<Pregunta> preguntas() {
+        List<Pregunta> result = null;
+        // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            //Escribimos la consulta en HQL
+            String hql = "from Pregunta";
+            Query query = session.createQuery(hql);
+            result = (List<Pregunta>)query.list();
+            tx.commit();
+        }
+        catch (Exception e) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           e.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return result;
+    }
+}
