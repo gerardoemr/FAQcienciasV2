e209684c1055f2fefbd62eb66a94176c0494d3c0
NoxCy <noxorum@ciencias.unam.mx>
2018-04-12 21:48:08 -0500

    Pequeños cambios en el aspecto del sistema

--- src/main/java/Controlador/Login.java
+++ src/main/java/Controlador/Login.java
@@ -1,27 +1,45 @@
 package Controlador;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import modelo.Usuario;
 import modelo.UsuarioDAO;
+import org.primefaces.PrimeFaces;
 
 /**
- *
+ * Login.java:
+ * 
+ *  Controlador para el caso de uso  
+ * 
  * @author gerardo
+ * @version 1.0, 10/04/2018
+ * @see documento "Especificación de Diseño de Software"
+ * @since jdk 7.0 
  */
 @ManagedBean
 @SessionScoped
-public class Login {
+public class Login implements Serializable{
     
+    /**
+     * Cadena que almacena el correo de la sesión actual.
+     */
     private String correo;
-    private String contrasena;
     
+    /**
+     * Cadena para validar con la contraseña registrada.
+     */
+    private String contrasena;
+
     public String getCorreo() {
         return correo;
     }
-
+    
     public void setCorreo(String correo) {
         this.correo = correo;
     }
@@ -34,34 +52,42 @@ public class Login {
         this.contrasena = contrasena;
     }
     
-    public String login() {
-        UsuarioDAO dao = new UsuarioDAO();
-        Usuario u = dao.busca(correo);
+    /**
+     * Método principal para iniciar sesión
+     * @return cadena que redirige al usuario a la vista InicioIH si la autenticación fue exitosa, null en otro caso.
+     * @throws IOException 
+     */
+    public String login() throws IOException {
+        UsuarioDAO dao = new UsuarioDAO(); 
+        Usuario u = dao.busca(correo);//en esta línea se busca se verifica si el correo es de un usuario registrado.
+        FacesMessage message; //se crea este mensaje para mandar alertas en la vista.
         FacesContext context = FacesContext.getCurrentInstance();
-
-        if (u == null) {
-            context.addMessage(null, new FacesMessage("El correo que ingresó no está registrado, intente de nuevo"));
-            correo = null;
-            contrasena = null;
-            return null;
-        } else if(u.getContrasena().equals(this.contrasena)){
-            context.getExternalContext().getSessionMap().put("user", u);
-            
-            return "user/InicioIH";
-        } else {
-            context.addMessage(null, new FacesMessage("Contraseña incorrecta"));
-            correo = null;
-            contrasena = null;
-            return null; 
+        boolean loggedIn;//bandera para determinar si el inicio de sesión fue exitoso.
+         
+        
+        if(u != null && contrasena.equals(u.getContrasena())) { //credenciales válidas
+            loggedIn = true;
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", u.getNombre());//mensaje de bienvenida
+            context.getExternalContext().getSessionMap().put("user", u);//se agrega la información del usuario al contexto.
+        } else {//credenciales inválidas
+            loggedIn = false;
+            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Credenciales inválidas");//mensaje de error
         }
-    }
-
-    public String logout() {
-        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        return "/index";
+         
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
+        String s =  (loggedIn) ? "/user/InicioIH": null; //cadena que redirige al usuario a InicioIH si el inicio de sesión fue exitoso.
+        return s;
     }
     
-    public String regreso() {
-        return "index";
+    /**
+     * Método para cerrar sesión.
+     * @return cadena que cierra la sesión del usuario actual y lo redirige a la página index.
+     * @throws IOException 
+     */
+    public String logout() throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();//cierra la sesión
+        FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+        return "/index";//se redirige al usuario a la página principal (index.xhtml)
     }
 }
