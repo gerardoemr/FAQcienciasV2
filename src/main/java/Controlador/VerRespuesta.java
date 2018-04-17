a4d2430a187975ac6197de34f3a960ca1aeddaef
NoxCy <noxorum@ciencias.unam.mx>
2018-04-16 21:03:28 -0500

    ...

---
+++ src/main/java/Controlador/VerRespuesta.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import modelo.Pregunta;
+import modelo.Respuesta;
+import modelo.RespuestaDAO;
+import javax.faces.context.FacesContext;
+import modelo.PreguntaDAO;
+/**
+ *
+ * @author Admin
+ */
+
+@ManagedBean
+@RequestScoped
+public class VerRespuesta {
+    private List<Respuesta> respuestas;
+    private Pregunta pregunta;
+
+    public List<Respuesta> getRespuestas() {
+        return respuestas;
+    }
+    
+    
+    
+    
+    public String listarRespuestas(Pregunta p){
+    this.pregunta=p;
+    FacesContext context = FacesContext.getCurrentInstance();
+     context.getExternalContext().getSessionMap().put("pregunta", p);
+     PreguntaDAO a=new PreguntaDAO();
+     a.aumentarVista(pregunta);
+     RespuestaDAO lib = new RespuestaDAO();
+    respuestas= lib.respuestas(pregunta);
+    return "verrespuestas";
+    }
+}
